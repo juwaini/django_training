63c864cdc11441cffd0ed82d6c941380b6a03a17
Juwaini <juwaini@gmail.com>
2018-12-20 16:38:14 +0800

    End the training

--- book/models.py
+++ book/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+from django.utils.timezone import now
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
 
 class Book(models.Model):
-    ...
+    title = models.CharField(max_length=100)
+    published_date = models.DateField(default=now)
+    author = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
--- book/views.py
+++ book/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import HttpResponse
+
+from .models import Book
+
+
+def book_list(request):
+    b = Book.objects.all()
+    return HttpResponse(b)
+
+
+def book_details(request):
+    ...
+
+
+def book_update(request, pk):
+    ...
